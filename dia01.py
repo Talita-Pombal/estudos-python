c86d0126dbae061604d1cf4b0d3729abd4902e9b
Talita Pombal <tapombal@gmail.com>
2026-05-28 21:40:40 -0300

    primeiro commit - exercicios dia 01 e 02

---
+++ dia01.py
@@ -0,0 +1,17 @@
+# Informações básicas
+nome = "seu nome aqui"
+idade = 0 # coloca sua idade
+area_interesse = "back-end e IA"
+
+print (f"Meu nome é {nome}, tenho {idade} anos.")
+print (f"Estou migrando para {area_interesse}.")
+
+# Cálculo simples
+horas_por_dia = 2
+dias_por_semana = 5
+semanas = 12
+meses = 3
+
+total_horas = horas_por_dia * dias_por_semana * semanas
+print(f"Em 12 semanas vou estudar {total_horas} horas.")
+print(f"Em 3 meses vou estudar {total_horas} horas.")
---
+++ dia02.py
@@ -0,0 +1,11 @@
+# Simulador de meia de estudos
+
+meta_horas = 120
+horas_estudadas = 10
+horas_por_dia = 2
+
+dias_necessarios = (meta_horas - horas_estudadas) / horas_por_dia
+
+print(f"Meta total: {meta_horas} horas")
+print(f"Horas já estudadas: {horas_estudadas}")
+print(f"Dias necessarios para concluir: {dias_necessarios}")
